be3be57c141d444c9b865d3f57f88c31cafec1dd
Chethan <chethans0252@gmail.com>
2026-04-07 22:10:35 +0530

    fixes

--- src/components/GD.jsx
+++ src/components/GD.jsx
@@ -5,9 +5,9 @@ export default function GD() {
     return (
         <>
             <div className='grid grd'>
-                <img className='grdimg' alt='github error' src="https://github-readme-stats.vercel.app/api?username=chethan025&commits_year=2025&show_icons=true&theme=dark" />
+                <img className='grdimg' alt='github error' src="https://github-readme-stats-blue-theta-15.vercel.app/api?username=chethan025&commits_year=2025&show_icons=true&theme=dark" />
                 <img className='grdimg' alt='github error' src="https://github-readme-streak-stats.herokuapp.com/?user=chethan025&theme=dark" />
-                <img className='grdimg' alt='github error' src="https://github-readme-stats.vercel.app/api/top-langs/?username=chethan025&layout=compact&theme=dark" />
+                <img className='grdimg' alt='github error' src="https://github-readme-stats-blue-theta-15.vercel.app/api/top-langs/?username=chethan025&layout=compact&theme=dark" />
                         
             </div>
         </>
